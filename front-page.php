817f58ba6af1579062b01d74bbc50c833465e7a6
Egregorion <gregory.fourmaux@gmail.com>
2022-05-17 16:25:05 +0200

    ajout du custom post type

--- front-page.php
+++ front-page.php
@@ -6,7 +6,15 @@ if ( have_posts() ) :
     while ( have_posts() ) : the_post(); ?>
         <div class="container">
             <h2><?php the_title() ?></h2>
-            <span class="pills"><?php the_category() ?></span>
+            <div><?php the_content() ?></div>
+            <div class="row">
+                <div class="col">
+                    <img class="img-fluid" src="<?php the_field('image_a_gauche') ?>" alt="">
+                </div>
+                <div class="col">
+                    <p><?php the_field('texte_a_droite') ?></p>
+                </div>
+            </div>
         </div>
     <?php endwhile; 
 endif; 
--- functions.php
+++ functions.php
@@ -10,5 +10,21 @@ function mon_theme_scripts() {
     wp_enqueue_script('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js', [], false, true);
 }
 
+function mon_theme_custom_types() {
+    register_post_type('boardgame', [
+        'labels' => [
+            'name' => 'Boardgames',
+            'singular_name' => 'Boardgame'
+        ],
+        'public' => true,
+        'menu_position' => 3,
+        'menu_icon' =>'dashicons-buddicons-activity',
+        'supports' => ['title', 'editor', 'thumbnail'],
+        'show_in_rest' => true,
+        'has_archive' => true
+    ]);
+}
+
 add_action('after_setup_theme', 'montheme_setup');
+add_action('init', 'mon_theme_custom_types');
 add_action('wp_enqueue_scripts', 'mon_theme_scripts');
